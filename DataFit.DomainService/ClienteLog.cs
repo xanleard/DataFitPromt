1b07b0e0ee3a4b14f574f1e64709a0271e74918b
Rudy.Cardona <rudy.cardona@grupoleitz.com>
2020-10-29 23:19:30 -0600

    Se corrigio logica de los test para clietnes y productos

---
+++ DataFit.DomainService/ClienteLog.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace DataFit.DomainService
+{
+    public class ClienteLog
+    {
+
+        public bool ValidarNombre(string nombre)
+        {
+            if (nombre != "")
+            {
+                return true;
+            }
+            return false;
+
+        }
+
+    }
+}
