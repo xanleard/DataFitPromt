d5c9c735b03f3b24f6ad9c8ba0b9a95a812282ee
Rudy.Cardona <rudy.cardona@grupoleitz.com>
2020-11-06 16:04:59 -0600

    se agregaron los test correspondientes de facturas

---
+++ DataFit.DomainService/FacturaLog.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataFit.DomainService
+{
+    public class FacturaLog
+    {
+        public bool ValidarTotalFactura(int total)
+        {
+            if (total > 0)
+            {
+                return true;
+            }
+            return false;
+
+        }
+
+        public bool ValidarSubTotalFactura(int subtotal)
+        {
+            if (subtotal > 0)
+            {
+                return true;
+            }
+            return false;
+
+        }
+
+    }
+}
