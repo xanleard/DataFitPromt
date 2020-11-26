e6b4e52531c82fb00f88378fd0449c22d984014e
Rudy.Cardona <rudy.cardona@grupoleitz.com>
2020-11-26 14:08:13 -0600

    implementacion de BDD

---
+++ SpecFlowDataFit/Validation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SpecFlowDataFit
+{
+    public class Validation
+    {
+        public int FirstNumber { get; set; }
+        public int SecondNumber { get; set; }
+
+        public int Add()
+        {
+            return FirstNumber + SecondNumber;
+        }
+    }
+}
